0632e25ce7d17f1a99cb8469664e77960ba350b3
koyasaeki <koyasaeki@gmail.com>
2021-11-20 19:39:04 +0900

    📦 ビルド設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/si-grenoble/",
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/main.ts"),
+      name: "SiGrenoble",
+      fileName: (format) => `si-grenoble.${format}.js`
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "vue"
+        }
+      }
+    }
+  },
   plugins: [vue()],
 });
